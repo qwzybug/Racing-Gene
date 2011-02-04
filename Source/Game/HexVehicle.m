fee9065bf2741abb1141a13ca003a8b64afeae0a
Devin Chalmers <devin.chalmers@gmail.com>
2011-02-04 03:06:41 -0500

    More critter hack

--- Source/Game/HexVehicle.m
+++ Source/Game/HexVehicle.m
@@ -64,6 +64,15 @@ springForce(cpConstraint *spring, cpFloat dist)
 	
 	self.mainBody = [[[CPhysicsBody alloc] initWithMass:100 inertia:100] autorelease];
 	self.mainBody.position = (cpVect){ 0, 50 };
+		
+	CPhysicsShape *theBodyShape = [CPhysicsShape boxShapeWithBody:self.mainBody size:(CGSize){ 5, 5 }];
+	theBodyShape.group = 1;
+	theBodyShape.elasticity = 1.4;
+	theBodyShape.friction = 0.5;
+	[self.mainBody addShape:theBodyShape];
+	
+	int lastW = 5;
+	int lastLastW = 5;
 	
 	CPhysicsBody *lastBody = self.mainBody;
 	CPhysicsBody *lastLastBody = self.mainBody;
@@ -77,56 +86,8 @@ springForce(cpConstraint *spring, cpFloat dist)
 	hex_gene gene;
 	do {
 		gene = self.critter.genome[gene_num];
-		
 		int sz = gene.size;
 		
-		CPhysicsBody *cell = [[[CPhysicsBody alloc] initWithMass:10 inertia:10] autorelease];
-		cell.position = p;
-		[self.mainBody addSubbody:cell];
-		
-		CPhysicsShape *cellShape = [CPhysicsShape ballShapeWithBody:cell radius:sz];
-		cellShape.group = 1;
-		cellShape.elasticity = 1.4;
-		cellShape.friction = 10.0;
-		[cell addShape:cellShape];
-		
-		cpVect pivotP = (cpVect){ p.x - lastP.x, p.y - lastP.y };
-		CPhysicsConstraint *pivot = [[[CPhysicsConstraint alloc] initWithConstraint:cpPivotJointNew2(lastBody.body, cell.body, pivotP, (cpVect){ 0, 0 })] autorelease];
-		[self.mainBody addConstraint:pivot];
-		
-		// spring between this point and the last one…
-		float springLength = sqrt(pivotP.x * pivotP.x + pivotP.y * pivotP.y);
-		cpConstraint *spring = cpDampedSpringNew(lastBody.body, cell.body, (cpVect){ 0, 0 }, (cpVect){ 0, 0 }, springLength, 100.0, 0.5);
-		cpDampedSpringSetSpringForceFunc(spring, springForce);
-		CPhysicsConstraint *springConstraint = [[[CPhysicsConstraint alloc] initWithConstraint:spring] autorelease];
-		[self.mainBody addConstraint:springConstraint];
-		
-		// and between this point and the origin
-		cpVect off = (cpVect){ p.x - lastLastP.x, p.y - lastLastP.y };
-		float spring2Length = sqrt(off.x * off.x + off.y * off.y);
-		cpConstraint *spring2 = cpDampedSpringNew(lastLastBody.body, cell.body, (cpVect){ 0, 0 }, (cpVect){ 0, 0 }, spring2Length, 500.0, 0.5);
-		cpDampedSpringSetSpringForceFunc(spring2, springForce);
-		CPhysicsConstraint *spring2Constraint = [[[CPhysicsConstraint alloc] initWithConstraint:spring2] autorelease];
-		[self.mainBody addConstraint:spring2Constraint];
-		
-		// time to freak out
-//		CPhysicsConstraint *motor = [[[CPhysicsConstraint alloc] initWithConstraint:cpSimpleMotorNew(cell.body, lastBody.body, kWheelRate)] autorelease];
-//		[self.mainBody addConstraint:motor];
-
-		CSceneGeometry *cellNode = [CSceneGeometry circleGeometryNodeWithRadius:cpCircleShapeGetRadius(cell.shape.shape)];
-		cellShape.userInfo = cellNode;
-		
-		[nodes addObject:cellNode];
-		
-		lastLastBody = lastBody;
-		lastBody = cell;
-		
-		// compute next body position
-		if (gene.next_hex != -1)
-			sz = (sz + self.critter.genome[gene_num + 1].size) / 2;
-		
-		lastLastP = lastP;
-		lastP = p;
 		switch (gene.next_hex) {
 			case 0:
 				p = (cpVect){p.x - 2 * V_V(sz).x, p.y - 2 * V_V(sz).y};
@@ -147,15 +108,56 @@ springForce(cpConstraint *spring, cpFloat dist)
 				p = (cpVect){p.x + V_H(sz).x - V_V(sz).x, p.y + V_H(sz).y - V_V(sz).y};
 				break;
 		}
-			
+		
+		cpVect lastV = (cpVect){ p.x - lastP.x, p.y - lastP.y };
+		cpVect lastLastV = (cpVect){ p.x - lastLastP.x, p.y - lastLastP.y };
+		
+		CGFloat lastD = sqrt(lastV.x * lastV.x + lastV.y * lastV.y);
+		CGFloat lastLastD = sqrt(lastLastV.x * lastLastV.x + lastLastV.y * lastLastV.y);
+		
+		CPhysicsBody *cell = [[[CPhysicsBody alloc] initWithMass:10 inertia:10] autorelease];
+		cell.position = p;
+		[self.mainBody addSubbody:cell];
+		
+		CPhysicsShape *cellShape = [CPhysicsShape ballShapeWithBody:cell radius:sz];
+		cellShape.group = 1;
+		cellShape.elasticity = 1.4;
+		cellShape.friction = 10.0;
+		[cell addShape:cellShape];
+		
+		// spring between this point and the last one…
+		cpConstraint *spring = cpDampedSpringNew(lastBody.body, cell.body, (cpVect){ 0, 0 }, (cpVect){ 0, 0 }, lastD, 500.0, 0.1);
+		cpDampedSpringSetSpringForceFunc(spring, springForce);
+		CPhysicsConstraint *springConstraint = [[[CPhysicsConstraint alloc] initWithConstraint:spring] autorelease];
+		[self.mainBody addConstraint:springConstraint];
+		
+		// and between this point and the origin
+		cpConstraint *spring2 = cpDampedSpringNew(lastLastBody.body, cell.body, (cpVect){ 0, 0 }, (cpVect){ 0, 0 }, lastLastD, 500.0, 0.1);
+		cpDampedSpringSetSpringForceFunc(spring2, springForce);
+		CPhysicsConstraint *spring2Constraint = [[[CPhysicsConstraint alloc] initWithConstraint:spring2] autorelease];
+		[self.mainBody addConstraint:spring2Constraint];
+		
+		CSceneGeometry *cellNode = [CSceneGeometry circleGeometryNodeWithRadius:sz];
+		cellShape.userInfo = cellNode;
+		
+		[nodes addObject:cellNode];
+		
+		lastLastBody = lastBody;
+		lastBody = cell;
+		
+		lastLastW = lastW;
+		lastW = lastD;
+		
+		// compute next body position
+		if (gene.next_hex != -1)
+			sz = (sz + self.critter.genome[gene_num + 1].size) / 2;
+		
+		lastLastP = lastP;
+		lastP = p;
+
+		
 		gene_num++;
-	} while (gene.next_hex != -1);
-	
-	CPhysicsShape *theBodyShape = [CPhysicsShape boxShapeWithBody:self.mainBody size:(CGSize){ 5, 5 }];
-	theBodyShape.group = 1;
-	theBodyShape.elasticity = 1.4;
-	theBodyShape.friction = 0.5;
-	[self.mainBody addShape:theBodyShape];
+	} while (gene.next_hex != -1 && gene_num);
 	
 	CSceneGeometry *theChassisNode = [CSceneGeometry flatGeometryNodeWithCoordinatesBuffer:[self.mainBody.shape vertexBuffer]];
     theBodyShape.userInfo = theChassisNode;
